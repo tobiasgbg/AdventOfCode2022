86213c544e0b454a54781d39a415bc20c3c39a82
Tobias Adriansson <tobias.gbg@gmail.com>
2023-01-06 11:26:23 +0100

    Solve day4 part 2

--- src/main/groovy/day4part1.groovy
+++ src/main/groovy/day4part1.groovy
@@ -47,23 +47,32 @@ static void main(String[] args) {
     File file = new File('../../../input/day4part1.txt')
     Integer result = 0
 
-    file.eachLine {if (CampCleanup.isIntersecting(it)){result++}}
+    file.eachLine {if (CampCleanup.fullyContains(it)){result++}}
 
     println("Sum ${result}")
 }
 
 class CampCleanup {
-    static def isIntersecting(String a) {
+    static def fullyContains(String a) {
         return containsOther(a.split(",")[0],a.split(",")[1])
                 || containsOther(a.split(",")[1],a.split(",")[0])
     }
 
-    static def isIntersecting(String a, String b) {
+    static def fullyContains(String a, String b) {
         return containsOther(a,b) || containsOther(b,a)
     }
 
     static def containsOther(String a, String b) {
         return (((a.split("-")[0] as Integer) <= (b.split("-")[0] as Integer))
                 && ((b.split("-")[1] as Integer) <= (a.split("-")[1]) as Integer))
+    }
+
+    static def isIntersecting(String a) {
+        return intersectsOther(a.split(",")[0],a.split(",")[1])
+    }
+
+    static def intersectsOther(String a, String b) {
+        return (a.split("-")[1] as Integer) >= (b.split("-")[0] as Integer) &&
+                (a.split("-")[0] as Integer) <= (b.split("-")[1] as Integer)
     }
 }
--- src/test/groovy/Day4Specification.groovy
+++ src/test/groovy/Day4Specification.groovy
@@ -12,9 +12,9 @@ class Day4Specification extends Specification {
         "2-8" | "2-8"  | true
     }
 
-    def "is intersecting"(String a, String b, boolean c) {
+    def "is fully containing"(String a, String b, boolean c) {
         expect:
-        CampCleanup.isIntersecting(a, b) == c
+        CampCleanup.fullyContains(a, b) == c
 
         where:
         a     |  b     |  c
@@ -22,9 +22,9 @@ class Day4Specification extends Specification {
         "2-4" | "6-8"  | false
     }
 
-    def "is intersecting full string"(String a, boolean b) {
+    def "is fully containing whole string"(String a, boolean b) {
         expect:
-        CampCleanup.isIntersecting(a) == b
+        CampCleanup.fullyContains(a) == b
 
         where:
         a           |  b
@@ -34,5 +34,34 @@ class Day4Specification extends Specification {
         "2-8,3-7"   | true
         "6-6,4-6"   | true
         "2-6,4-8"   | false
+    }
+
+    def "is intersecting whole string"(String a, boolean b) {
+        expect:
+        CampCleanup.isIntersecting(a) == b
+
+        where:
+        a           |  b
+        "2-4,6-8"   | false
+        "2-3,4-5"   | false
+        "5-7,7-9"   | true
+        "2-8,3-7"   | true
+        "6-6,4-6"   | true
+        "2-6,4-8"   | true
+        "9-71,8-8"  | false
+    }
+
+    def "intersects other"(String a, String b, boolean c) {
+        expect:
+        CampCleanup.intersectsOther(a, b) == c
+
+        where:
+        a     |  b      | c
+        "2-4" | "6-8"   | false
+        "2-3" | "4-5"   | false
+        "5-7" | "7-9"   | true
+        "2-8" | "3-7"   | true
+        "6-6" | "4-6"   | true
+        "2-6" | "4-8"   | true
     }
 }
